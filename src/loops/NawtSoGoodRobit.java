24c5e5e2ddce00fcd291d6bf0b2538b5f87df2cf
ilabstaff <ilabstaff@172.16.116.81>
2019-04-24 19:25:22 -0700

    PERIDOT

--- src/loops/NawtSoGoodRobit.java
+++ src/loops/NawtSoGoodRobit.java
@@ -5,17 +5,30 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class NawtSoGoodRobit {
-static Robot rob=new Robot();
+	static Robot rob = new Robot();
 
-public static void main(String[] args) {
-	for(int i=500; i<500;i++)
-		rob.setSpeed(500);
-	rob.penDown(); 
-	rob.setAngle(90);
-	 rob.move(100);
-	 Entschuldigung!
-	 ;
-     rob.setPenColor(Color.pink);        
-     
-}
+	public static void main(String[] args) {
+		for (int i = 0; i < 50000; i++) {
+			rob.setRandomPenColor();
+			rob.setRandomPenColor();
+			rob.setRandomPenColor();
+			rob.setRandomPenColor();
+			rob.setRandomPenColor();
+			rob.setSpeed(50000000);
+			rob.setPenWidth(5);
+
+			rob.sparkle();
+			rob.penDown();
+			rob.setPenWidth(5);
+			rob.move(300);
+			rob.turn(500);
+			rob.setPenColor(Color.pink);
+
+		}
+
+		 
+		
+		rob.setPenColor(Color.pink);
+
+	} 
 }
